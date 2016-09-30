43eae1dbec0d41e53d2c08285a9b95f6f6e50bdf
Barry Devereux <barry@users.noreply.github.com>
2016-09-30 18:45:42 +0100

    Delete runme.bash

--- runme.bash
+++
@@ -1,9 +0,0 @@
-# To install and test everything on the EC2 instance
-sudo apt-get update;
-sudo apt-get install git;
-git clone https://github.com/barry/aws-tensorflow-setup/;
-./aws-tensorflow-setup/setup-aws-tensorflow.bash ;
-mkdir tf_test_mnist;
-cd tf_test_mnist/;
-wget https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/models/image/mnist/convolutional.py;
-python convolutional.py;
